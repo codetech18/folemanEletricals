da4bc49dcb85ddb9cc8ef2ec6f8d59a7b3a29f80
codetech18 <akoredealao18@gmail.com>
2026-05-20 19:36:23 +0100

    still fixing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,43 @@
-import { useEffect, useState } from 'react';
-import { ContactSection } from './components/ContactSection';
-import { CustomCursor } from './components/CustomCursor';
-import { Hero } from './components/Hero';
-import { Navbar } from './components/Navbar';
-import { PageLoader } from './components/PageLoader';
-import { ProblemSection } from './components/ProblemSection';
-import { ProcessSection } from './components/ProcessSection';
-import { ServicesSection } from './components/ServicesSection';
-import { StatsSection } from './components/StatsSection';
-import { useLenis } from './hooks/useLenis';
-import { useScrollTriggerRefresh } from './hooks/useScrollTrigger';
-import { Works } from './pages/Works';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Works } from "./pages/Works";
+import { CustomCursor } from "./components/CustomCursor";
+import { Navbar } from "./components/Navbar";
+import { PageLoader } from "./components/PageLoader";
+import { Hero } from "./components/Hero";
+import { ProblemSection } from "./components/ProblemSection";
+import { ServicesSection } from "./components/ServicesSection";
+import { StatsSection } from "./components/StatsSection";
+import { ProcessSection } from "./components/ProcessSection";
+import { ContactSection } from "./components/ContactSection";
+import { useLenis } from "./hooks/useLenis";
+import { useScrollTriggerRefresh } from "./hooks/useScrollTrigger";
+
+function Home() {
+  return (
+    <main>
+      <Hero />
+      <ProblemSection />
+      <ServicesSection />
+      <StatsSection />
+      <ProcessSection />
+      <ContactSection />
+    </main>
+  );
+}
 
 export default function App() {
-  const [pathname, setPathname] = useState(() => window.location.pathname);
   useLenis();
   useScrollTriggerRefresh();
 
-  useEffect(() => {
-    const update = () => setPathname(window.location.pathname);
-    window.addEventListener('popstate', update);
-    return () => window.removeEventListener('popstate', update);
-  }, []);
-
-  const isWorksPage = pathname === '/works';
-
   return (
-    <>
+    <BrowserRouter>
       <PageLoader />
-      {isWorksPage ? null : <CustomCursor />}
+      <CustomCursor />
       <Navbar />
-      {isWorksPage ? (
-        <Works />
-      ) : (
-        <main>
-          <Hero />
-          <ProblemSection />
-          <ServicesSection />
-          <StatsSection />
-          <ProcessSection />
-          <ContactSection />
-        </main>
-      )}
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/works" element={<Works />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
